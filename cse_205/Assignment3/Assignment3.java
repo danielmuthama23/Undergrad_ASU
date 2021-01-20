5f037e55e7a3c948a2ac1c02294bcc687685784d
tfich <tfichiera@gmail.com>
2021-01-19 17:57:57 -0700

    Assignment3

---
+++ cse_205/Assignment3/Assignment3.java
@@ -0,0 +1,36 @@
+/* 
+Assignment #: 3
+Arizona State University - CSE205
+Name: Tyler Fichiera
+StudentID: 1218959911
+Lecture: T/Th - 4:30
+Description: Five software engineering principles and examples
+
+1. PUBLIC – Software engineers shall act consistently with the public interest.
+Ex./ A web developer designs a search engine to promote incorrect information about
+    the covid-19 vaccine which misinforms the public about their health and safety.
+
+2. CLIENT AND EMPLOYER – Software engineers shall act in a manner that is in the best
+    interests of their client and employer consistent with the public interest.
+Ex./ A data scientist is writing code that analyzes a client's database. The developer
+    alters the data and produces incorrect statistics. This is a violation of this
+    policy.
+
+3. PRODUCT – Software engineers shall ensure that their products and related
+    modifications meet the highest professional standards possible.
+Ex./ An engineer at Tesla is writing code for a self-driving car and is implementing
+    encryption. The engineer decides to take the easy route and implements faulty
+    and out-dated encryption which puts the passangers at risk.
+    
+4. JUDGMENT – Software engineers shall maintain integrity and independence in their
+    professional judgment.
+Ex./ A junior software developer is told by his boss to steal another company's code
+    without permission. The junior dev does as he is told even though he knows what
+    he is doing is wrong.
+
+5. MANAGEMENT – Software engineering managers and leaders shall subscribe to and
+promote an ethical approach to the management of software development and maintenance.
+Ex./ A web developer's API suddenly stops working while in production so the maintainer
+    decides to steal another company's API and forward it through his own service. It is
+    the maintainer's job to make sure his own web service is always available.
+*/
